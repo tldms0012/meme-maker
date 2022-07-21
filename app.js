3b54f2bbe6826c924812d413fa7a2a038d952f75
tldms0012 <81214004+tldms0012@users.noreply.github.com>
2022-07-21 21:33:46 +0900

    meme_maker_4

--- app.js
+++ app.js
@@ -3,26 +3,26 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = 2;
+let isPainting = false;
 
-const colors = [
-    "#cd84f1",
-    "#ffcccc",
-    "#ff4d4d",
-    "#ffaf40",
-    "#fffa65",
-    "#32ff7e",
-    "#7efff5",
-    "#18dcff",
-    "#7d5fff",    
-]
-
-function onClick(event){
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    const color = colors[Math.floor(Math.random() * colors.length)];
-    ctx.strokeStyle = color;
-    ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.stroke();
+function onMove(event){
+    if(isPainting){
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        return;
+    }
+    ctx.moveTo(event.offsetX, event.offsetY);
+}
+function startPainting(event){
+    isPainting = true;
+}
+function cancelPainting(event){
+    isPainting = false;
 }
 
-canvas.addEventListener("mousemove", onClick);
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
+//document.addEventListener("mouseup", onMouseUp);
