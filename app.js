32e41df73be817a124f3dfcb4e6a0a8e16edcd9b
tldms0012 <81214004+tldms0012@users.noreply.github.com>
2022-07-24 23:28:01 +0900

    meme_maker_5

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
+const eraserBtn = document.getElementById("eraser-btn");
+const destroyBtn = document.getElementById("destroy-btn");
+const modeBtn = document.getElementById("mode-btn");
+const colorOptions = Array.from(
+    document.getElementsByClassName("color-option")
+);
+const color = document.getElementById("color");
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+
 canvas.width = 800;
 canvas.height = 800;
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 let isPainting = false;
+let isFilling = false;
 
 function onMove(event){
     if(isPainting){
@@ -18,11 +31,57 @@ function startPainting(event){
 }
 function cancelPainting(event){
     isPainting = false;
+    ctx.beginPath();
 }
-
+function onLineWidthChange(event){
+    ctx.lineWidth = event.target.value;
+}
+function onColorChage(event){
+    ctx.strokeStyle = event.target.value;
+    ctx.fillStyle - event.target.value;
+}
+function onColorClick(event){
+    const colorValue = event.target.dataset.color;
+    ctx.strokeStyle = colorValue;
+    ctx.fillStyle = colorValue;
+    color.value = colorValue;
+}
+function onModeClick(){
+ if(isFilling){
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+ } else {
+    isFilling = true;
+    modeBtn.innerText = "Draw";
+ }
+}
+function onCanvasClick(){
+    if(isFilling){
+        ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    }
+}
+function onDestroyBtn(){
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+function onEraserClick(){
+    ctx.strokeStyle = "white";
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+}
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
 //document.addEventListener("mouseup", onMouseUp);
+canvas.addEventListener("click", onCanvasClick);
+
+lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChage);
+
+colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
+
+modeBtn.addEventListener("click", onModeClick);
+destroyBtn.addEventListener("click", onDestroyBtn);
+eraserBtn.addEventListener("click", onEraserClick);
